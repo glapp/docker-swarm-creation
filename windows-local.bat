5e1a86b8854e8ed157e135aaad69c3ee1df6cda3
Fabio <islerfab@gmail.com>
2015-12-15 11:29:06 +0100

    added restart policy for kvstore

--- windows-local.bat
+++ windows-local.bat
@@ -2,7 +2,7 @@ docker-machine rm swarm-agent-00 swarm-agent-01 swarm-master kvstore
 docker-machine create -d virtualbox kvstore
 FOR /f %%i IN ('docker-machine ip kvstore') DO SET KVSTORE=%%i
 FOR /f "tokens=*" %%i IN ('docker-machine env --shell=cmd kvstore') DO %%i
-docker run -d --net=host progrium/consul --server -bootstrap-expect 1
+docker run -d --net=host --restart always progrium/consul --server -bootstrap-expect 1
 docker-machine create -d virtualbox --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-master --swarm-discovery consul://%KVSTORE%:8500 swarm-master
 FOR /f %%i IN ('docker-machine ip swarm-master') DO SET SWARM_MASTER_IP=%%i
 FOR /f "tokens=*" %%i IN ('docker-machine env --shell=cmd swarm-master') DO %%i
