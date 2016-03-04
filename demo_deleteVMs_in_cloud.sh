de033f0d723299d195a47290a3ca486aaf5d01e8
patanric <patane_riccardo@gmx.net>
2016-03-04 20:22:44 +0100

    added delete file and prometheus file

---
+++ demo_deleteVMs_in_cloud.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting script..."
+docker-machine rm -f kvstore
+docker-machine rm -f swarm-master-do
+docker-machine rm -f swarm-agent-do-01
+docker-machine rm -f swarm-agent-aws-01
+docker-machine rm -f prometheusVM-aws
+echo "End of script."
