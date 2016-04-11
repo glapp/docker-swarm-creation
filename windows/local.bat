593f3311eda3e93c718ef44204357cd35d4b96a4
Fabio <islerfab@gmail.com>
2016-04-11 11:04:22 +0300

    fixed issue for windows if hyper-v is installed but deactivated

--- windows/local.bat
+++ windows/local.bat
@@ -2,16 +2,16 @@ REM Remove old nodes
 docker-machine rm -y swarm-agent-00 swarm-agent-01 swarm-master kvstore
 
 REM Create & provision KV store
-docker-machine create -d virtualbox kvstore
+docker-machine create -d virtualbox --virtualbox-no-vtx-check kvstore
 FOR /f %%i IN ('docker-machine ip kvstore') DO SET KVSTORE=%%i
 FOR /f "tokens=*" %%i IN ('docker-machine env --shell=cmd kvstore') DO %%i
 docker run -d --restart=always --net=host progrium/consul --server -bootstrap-expect 1
 REM docker-machine ssh kvstore "echo 'ifconfig eth1 %KVSTORE% netmask 255.255.255.0 broadcast 192.168.99.255 up' | sudo tee /var/lib/boot2docker/bootsync.sh > /dev/null"
 
 REM Create swarm nodes
-docker-machine create -d virtualbox --engine-label tier=1 --engine-label region=eu --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-master --swarm-discovery consul://%KVSTORE%:8500 swarm-master
-docker-machine create -d virtualbox --engine-label tier=2 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-discovery consul://%KVSTORE%:8500 swarm-agent-00
-docker-machine create -d virtualbox --engine-label tier=1 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-discovery consul://%KVSTORE%:8500 swarm-agent-01
+docker-machine create -d virtualbox --virtualbox-no-vtx-check --engine-label tier=1 --engine-label region=eu --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-master --swarm-discovery consul://%KVSTORE%:8500 swarm-master
+docker-machine create -d virtualbox --virtualbox-no-vtx-check --engine-label tier=2 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-discovery consul://%KVSTORE%:8500 swarm-agent-00
+docker-machine create -d virtualbox --virtualbox-no-vtx-check --engine-label tier=1 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth1:2376" --swarm --swarm-discovery consul://%KVSTORE%:8500 swarm-agent-01
 
 REM Define variables
 FOR /f %%i IN ('docker-machine ip swarm-master') DO SET SWARM_MASTER_IP=%%i
