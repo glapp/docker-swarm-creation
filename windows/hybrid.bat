c3573a07d45d98755a9e22d6987c21ed69575ab7
Fabio <islerfab@gmail.com>
2016-08-15 13:08:25 +0200

    changed AWS name for windows

--- windows/hybrid.bat
+++ windows/hybrid.bat
@@ -1,5 +1,5 @@
 REM Remove old nodes
-docker-machine rm -y DO-MASTER AWS DO-01 DO-02
+docker-machine rm -y DO-MASTER AWS-01 DO-01 DO-02
 
 REM Set credential variables
 FOR /f "tokens=*" %%i IN (..\credentials) DO SET %%i
@@ -18,11 +18,11 @@ REM Create swarm nodes
 docker-machine create -d digitalocean --engine-label tier=1 --engine-label region=eu --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth0:2376" --digitalocean-access-token=%DO_TOKEN%  --digitalocean-region=ams2 --digitalocean-image "debian-8-x64" --swarm --swarm-master --swarm-discovery consul://%KVSTORE%:8500 DO-MASTER
 docker-machine create -d digitalocean --engine-label tier=1 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth0:2376" --digitalocean-access-token=%DO_TOKEN% --digitalocean-region=nyc1 --digitalocean-image "debian-8-x64" --swarm --swarm-discovery consul://%KVSTORE%:8500 DO-01
 docker-machine create -d digitalocean --engine-label tier=2 --engine-label region=eu --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth0:2376" --digitalocean-access-token=%DO_TOKEN%  --digitalocean-region=ams2 --digitalocean-image "debian-8-x64" --digitalocean-size=1gb --swarm --swarm-discovery consul://%KVSTORE%:8500 DO-02
-docker-machine create -d amazonec2 --engine-label tier=1 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth0:2376" --amazonec2-access-key=%AWS_ACCESS_KEY% --amazonec2-secret-key=%AWS_SECRET_KEY% --amazonec2-region=us-east-1 --amazonec2-zone=a --swarm --swarm-discovery consul://%KVSTORE%:8500 AWS
+docker-machine create -d amazonec2 --engine-label tier=1 --engine-label region=us --engine-opt "cluster-store consul://%KVSTORE%:8500" --engine-opt "cluster-advertise eth0:2376" --amazonec2-access-key=%AWS_ACCESS_KEY% --amazonec2-secret-key=%AWS_SECRET_KEY% --amazonec2-region=us-east-1 --amazonec2-zone=a --swarm --swarm-discovery consul://%KVSTORE%:8500 AWS-01
 
 REM Define variables
 FOR /f %%i IN ('docker-machine ip DO-MASTER') DO SET DO_MASTER_IP=%%i
-FOR /f %%i IN ('docker-machine ip AWS') DO SET AWS_IP=%%i
+FOR /f %%i IN ('docker-machine ip AWS-01') DO SET AWS_01_IP=%%i
 FOR /f %%i IN ('docker-machine ip DO-01') DO SET DO_01_IP=%%i
 FOR /f %%i IN ('docker-machine ip DO-02') DO SET DO_02_IP=%%i
 
@@ -31,9 +31,9 @@ FOR /f "tokens=*" %%i IN ('docker-machine env DO-MASTER') DO %%i
 docker run -d --restart=always --name=registrator --volume=/var/run/docker.sock:/tmp/docker.sock -h DO-MASTER gliderlabs/registrator:latest -ip %DO_MASTER_IP% -internal consul://%KVSTORE%:8500
 docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=18080:8080 --detach=true google/cadvisor:latest
 
-REM Provision AWS
-FOR /f "tokens=*" %%i IN ('docker-machine env AWS') DO %%i
-docker run -d --restart=always --name=registrator --volume=/var/run/docker.sock:/tmp/docker.sock -h AWS gliderlabs/registrator:latest -ip %AWS_IP% -internal consul://%KVSTORE%:8500
+REM Provision AWS-01
+FOR /f "tokens=*" %%i IN ('docker-machine env AWS-01') DO %%i
+docker run -d --restart=always --name=registrator --volume=/var/run/docker.sock:/tmp/docker.sock -h AWS-01 gliderlabs/registrator:latest -ip %AWS_01_IP% -internal consul://%KVSTORE%:8500
 docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=18080:8080 --detach=true google/cadvisor:latest
 
 REM Provision DO-01
@@ -54,7 +54,7 @@ FOR /f "tokens=*" %%i IN ('docker-machine env kvstore') DO %%i
 docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=18080:8080 --detach=true google/cadvisor:latest
 COPY prometheus_template.yml prometheus.yml
 sed -i 's/MACHINE_1/%DO_MASTER_IP%/g' prometheus.yml
-sed -i 's/MACHINE_2/%AWS_IP%/g' prometheus.yml
+sed -i 's/MACHINE_2/%AWS_01_IP%/g' prometheus.yml
 sed -i 's/MACHINE_3/%DO_01_IP%/g' prometheus.yml
 sed -i 's/MACHINE_4/%DO_02_IP%/g' prometheus.yml
 docker-machine scp prometheus.yml kvstore:/tmp/prometheus.yml
